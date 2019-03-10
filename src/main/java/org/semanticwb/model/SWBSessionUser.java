34dd8653304e779d37fd6331e3eda940d4e1f752
Hasdai Pacheco <me@hasdaipacheco.com>
2019-03-09 22:06:38 -0600

    Updated code to use new attribute names in SemanticObject

--- src/main/java/org/semanticwb/model/SWBSessionUser.java
+++ src/main/java/org/semanticwb/model/SWBSessionUser.java
@@ -6,7 +6,7 @@
  * procesada por personas y/o sistemas, es una creación original del Fondo de Información y Documentación
  * para la Industria INFOTEC, cuyo registro se encuentra actualmente en trámite.
  *
- * INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público (‘open source’),
+ * INFOTEC pone a su disposición la herramienta SemanticWebBuilder a través de su licenciamiento abierto al público ('open source'),
  * en virtud del cual, usted podrá usarlo en las mismas condiciones con que INFOTEC lo ha diseñado y puesto a su disposición;
  * aprender de él; distribuirlo a terceros; acceder a su código fuente y modificarlo, y combinarlo o enlazarlo con otro software,
  * todo ello de conformidad con los términos y condiciones de la LICENCIA ABIERTA AL PÚBLICO que otorga INFOTEC para la utilización
@@ -17,52 +17,41 @@
  * de la misma.
  *
  * Si usted tiene cualquier duda o comentario sobre SemanticWebBuilder, INFOTEC pone a su disposición la siguiente
- * dirección electrónica:
- *  http://www.semanticwebbuilder.org
+ * dirección electrónica: http://www.semanticwebbuilder.org.mx
  */
 package org.semanticwb.model;
 
 import org.semanticwb.platform.SemanticObject;
 
-// TODO: Auto-generated Javadoc
 /**
  * The Class SWBSessionUser.
- * 
  * @author javier.solis
  */
-public class SWBSessionUser extends User
-{
-    
+public class SWBSessionUser extends User {
     /**
      * Instantiates a new sWB session user.
-     * 
+     *
      * @param rep the rep
      */
-    public SWBSessionUser(UserRepository rep)
-    {
-        super(new SemanticObject(rep.getSemanticObject().getModel(),User.swb_User));
+    public SWBSessionUser(UserRepository rep) {
+        super(new SemanticObject(rep.getSemanticObject().getModel(), User.swb_User));
     }
 
     /**
      * Update user.
-     * 
      * @param user the user
      */
-    public void updateUser(User user)
-    {
-        m_obj=user.getSemanticObject();
+    public void updateUser(User user) {
+        mObj = user.getSemanticObject();
     }
 
     @Override
-    public boolean equals(Object obj)
-    {
-        return m_obj.equals(obj);
+    public boolean equals(Object obj) {
+        return mObj.equals(obj);
     }
 
     @Override
-    public int hashCode()
-    {
-        return m_obj.hashCode();
-    }    
-
+    public int hashCode() {
+        return mObj.hashCode();
+    }
 }
